611d2dd3e98e5d64d91334ed2fa9ab29f89b2afe
negito6 <negito6@users.noreply.github.com>
2024-03-23 12:04:29 +0900

    unnecessary import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 // sqlite
 import 'dart:async';
-import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
